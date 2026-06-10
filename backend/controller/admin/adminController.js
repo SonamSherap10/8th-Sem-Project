0501233f6b507d2e460ca45d187ea5e766504986
Sonam Sherpa <sonamserpa3231@gmail.com>
2026-06-10 15:57:04 +0545

    Delete backend directory

--- backend/controller/admin/adminController.js
+++
@@ -1,9 +0,0 @@
-const userController = require("./adminUserController");
-const productController = require("./adminProductController");
-const salesController = require("./adminSalesController");
-
-module.exports = {
-  ...userController,
-  ...productController,
-  ...salesController,
-};
